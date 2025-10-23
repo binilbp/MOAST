c1cf5f5b63b57be62420afba0a7affe99e559d94
binil <biniloffcl@gmail.com>
2025-10-23 03:05:16 -0700

    Add text area widget and enable streaming output

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from libagent import get_commands
+from libagent import get_context_commands
 import asyncio 
 from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Static, TabbedContent, TabPane, Label, Input
+from textual.widgets import Footer, Static, TabbedContent, TabPane, Label, Input, TextArea
 from textual.containers import Container, Vertical, VerticalScroll
 
 
@@ -20,12 +20,18 @@ class NameContainer(Container):
         )
 
 
-class UserPrompt(VerticalScroll):
+class Terminal(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield Input(
                 placeholder = "Give your instruction here",
                 id = "userprompt"
         )
+
+        yield TextArea(
+                id = "terminaldisplay",
+                disabled = True
+        )
+
 
     @on (Input.Submitted)
     async def send_user_input(self) -> None:
@@ -33,26 +39,37 @@ class UserPrompt(VerticalScroll):
         user_input = input.value
         input.value = ""                                        #clear for nxt input
 
-        label = Label("Instruction sent !", id ="sent-msg")
-        self.mount(label)
-        label.styles.animate("opacity", value=0, duration=1.5)    #fadeout animation
+        #label = Label("Instruction sent !", id ="sent-msg")
+        #self.mount(label)
+        #label.styles.animate("opacity", value=0, duration=1.5)    #fadeout animation
 
-        response = get_commands(user_input = user_input, model = 'llama3.2')
-        command = Label(response['response'], id ="command")
-        self.mount(command)
-        await asyncio.sleep(2.5)  #give enough time for animation before the label is removed
-        label.remove()
+        response = get_context_commands(user_input = user_input, model = 'llama3.2')
+
+        command_area = self.query_one(TextArea)
+        command_area.disabled = True
+        command_area.clear()
+
+        for chunk in response:
+            if 'content' in chunk['message']:
+                command_area.insert(chunk['message']['content'])
+        command_area.insert("\n")
+        #enable editing in the generated content
+        command_area.disabled = False
+        #move the focus to command now
+        command_area.focus()
+        #await asyncio.sleep(2.5)  #give enough time for animation before the label is removed
+        #label.remove()
 
 
 
 
-class Body(Vertical):
+class Body(VerticalScroll):
     # main body container of the app
 
     def compose(self) -> ComposeResult:
         with TabbedContent():
             with TabPane("Terminal", id = "terminal"):
-                yield UserPrompt()
+                yield Terminal()
 
             with TabPane("Server", id = "server"):
                 yield Label("Server functionalities coming soon!")
--- libagent.py
+++ libagent.py
@@ -1,11 +1,31 @@
 import ollama
 import asyncio
 
+
+#intial AI model configuration prompt
+terminalmessages = [
+        {
+            'role': 'system',
+            'content ': 'You are a linux command assistant, who provides all the commands required for the user requested function. You only return correct commands for direct execution. In case a user function is not clear enough to provide commands. You should give out `echo "Please explain in more detail"`.Do not give any other conversation'
+        }
+]
+
 #FIX! use type hint for return
 def get_commands(model:str, user_input:str ):
+    #use this function to get non-context aware commands
     return ollama.generate(
             model = model,
             prompt = user_input
+    )
+
+def get_context_commands(model:str, user_input:str):
+    
+    terminalmessages.append({'role':'user','content':user_input})
+    # use this function to get context aware commands
+    return ollama.chat(
+            model = model,
+            messages = terminalmessages,
+            stream = True
     )
 
 
